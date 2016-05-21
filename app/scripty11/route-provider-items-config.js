758502c3a4c6fc885e9b8d1381e46e7f10c9b7b4
Richard Wardle <rjwardle2@gmail.com>
2016-05-21 16:12:56 +0100

    Add query string and route params examples.

--- app/scripty11/route-provider-items-config.js
+++ app/scripty11/route-provider-items-config.js
@@ -1,16 +1,27 @@
 angular.module("scripty11")
-    .config(function($locationProvider) {
+    .config(function ($locationProvider) {
         // $locationProvider.hashPrefix("!");
         // $locationProvider.html5Mode(true);
     })
-    .config(function($routeProvider) {
+    .config(function ($routeProvider) {
         $routeProvider
             .when("/", {
                 redirectTo: "/items"
+            })
+            .when("/search", {
+                controller: "ItemsController",
+                controllerAs: "index",
+                template: "<div class='well well-sm' ng-bind='location'></div>\
+                   <a ng-href='#/items?color=red'>Red items</a>"
             })
             .when("/items", {
                 controller: "ItemsController",
                 controllerAs: "index",
                 templateUrl: "scripty11/items/index.html"
+            })
+            .when("/items/:id", {
+                controller: "ItemController",
+                controllerAs: "index",
+                templateUrl: "scripty11/items/show.html"
             });
     });
--- app/scripty11/scripty11-controller.js
+++ app/scripty11/scripty11-controller.js
@@ -1,5 +1,9 @@
 angular.module('scripty11')
     .controller("ItemsController", function($location, Item) {
         this.location = $location.absUrl();
-        this.items = Item.query();
+        this.items = Item.query({color: $location.search().color});
+    })
+    .controller("ItemController", function($location, $routeParams, Item) {
+        this.location = $location.absUrl();
+        this.item = Item.get({id: $routeParams.id});
     });
